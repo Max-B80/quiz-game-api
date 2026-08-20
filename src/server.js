11371b0b199c3aa4eaf1ae2962cb9eca26bd3f1a
Max-B80 <mahsabamdadi@gmail.com>
2026-08-20 15:07:07 +0200

    feat: setup express server with questions route

---
+++ src/server.js
@@ -0,0 +1,14 @@
+import express from "express";
+const app = express();
+const PORT = 3000;
+
+
+
+app.listen(PORT,() => {
+    console.log(`Server is running on ://localhost:${PORT}`);
+});
+
+app.get("/",(req, res) => {
+    res.send("Welcome to the Quiz Game API!");
+});
+
